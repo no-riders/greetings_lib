4f60b854a421d07f5813451d81af6ced3a4c81d1
Yev <evlogosha@gmail.com>
2017-10-02 18:06:04 +0300

    jQuery support added, HTML markup added

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 var g = G$('Yev', 'Logosha');
-g.greet().setLang('ua').greet(true);
+g.greet().setLang('ua').greet(true).log();
+
+$('#login').click(function() {
+	var loginGtr = G$('Yev', 'Logosha');
+	$('#logindiv').hide();
+	loginGtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+});
